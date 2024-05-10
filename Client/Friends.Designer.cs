35633ebc831597aa7e4c5630004a2230efbb2d22
lhm19122004@gmail.com <lhm19122004@gmail.com>
2024-05-10 18:32:58 +0700

    Friends.cs

---
+++ Client/Friends.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace Client
+{
+    partial class Friends
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.userName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.lastSeen = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 178);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(251, 26);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(295, 170);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 34);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.userName,
+            this.lastSeen});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 224);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(560, 539);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // userName
+            // 
+            this.userName.Text = "User name";
+            this.userName.Width = 400;
+            // 
+            // lastSeen
+            // 
+            this.lastSeen.Text = "Last Logged In";
+            this.lastSeen.Width = 200;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Client.Properties.Resources.Help;
+            this.ClientSize = new System.Drawing.Size(584, 786);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader userName;
+        private System.Windows.Forms.ColumnHeader lastSeen;
+    }
+}
---
+++ Client/Friends.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
+
+
+namespace Client
+{
+    public partial class Friends : Form
+    {
+        IFirebaseConfig config = new FirebaseConfig
+        {
+            AuthSecret = "RupkJmOpqVD7u65mZo31WEtDRqKWpkN2Oj6UtbNT",
+            BasePath = "https://monopoly-8058b-default-rtdb.asia-southeast1.firebasedatabase.app/"
+        };
+
+        IFirebaseClient client;
+        public Friends()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FirebaseResponse res = await client.GetAsync("USER/");
+            dynamic IDs = res.ResultAs<dynamic>();
+            bool found = false;
+            listView1.Items.Clear();
+            foreach (var id in IDs)
+            {
+                string userID = id.Name;
+                FirebaseResponse userRes = await client.GetAsync("USER/" + userID);
+                User userdata = userRes.ResultAs<User>();
+                string userName = userdata.username;
+                string lastLoggedIn = userdata.last_logged_in;
+
+                if (SearchBox.Text.ToLower() == userName.ToLower())
+                {
+                    found = true;
+                    ListViewItem item = new ListViewItem(userName);
+                    item.SubItems.Add(lastLoggedIn.ToString());
+                    listView1.Items.Add(item);
+                    break;
+                }
+            }
+            if (!found)
+            {
+                MessageBox.Show("Player not found.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
